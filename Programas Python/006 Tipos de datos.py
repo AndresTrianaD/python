948a8d06a781379d6b6a4a4e735fe6cf6ac5645d
Andres Triana <73327110+AndresTrianaD@users.noreply.github.com>
2023-09-12 13:08:44 -0500

    Tipos de datos

---
+++ Programas Python/006 Tipos de datos.py
@@ -0,0 +1,27 @@
+#Tipo de dato entero o largo long es ta limitado por el espacio en menmoria de la pc
+#tipado dinamico 
+numero = 15
+print (numero, type(numero))
+
+#Tipo de dato flotante
+numero_flotante = 15.6
+print(numero_flotante, type(numero_flotante))
+
+#Tipo de numero complejo
+numero_complejo = 5 + 6j
+print(numero_complejo, type(numero_complejo))
+
+#Tipo de dato string
+nombre_string = "Andres"
+print(nombre_string,type(nombre_string))
+
+nombre_string = '''Andres'''
+print(nombre_string,type(nombre_string))
+
+#Tipo de dato Booleano
+verdadero_falso = 3 == 3
+print(verdadero_falso, type(verdadero_falso))
+
+verdadero_falso = 3 == 4
+print(verdadero_falso, type(verdadero_falso))
+
