33003d5e0973bfd5500c4aed7c74d4f20ececc92
Andres Triana <73327110+AndresTrianaD@users.noreply.github.com>
2023-09-11 22:57:30 -0500

    Comentarios

---
+++ Programas Python/005 Comentarios.py
@@ -0,0 +1,12 @@
+#Esto es un comentari
+print ("Hola")
+
+"Esto es otro comentario, las comillas son string no asignados por eso no son tomadas en cuenta"
+
+"""
+Otro tipo de comentario
+"""
+
+'''
+De la misma forma esto es comentari multilinea
+'''
