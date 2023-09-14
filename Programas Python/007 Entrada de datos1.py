8bd597bfd2fe982acc1c321e55a444a190c3bf12
Andres Triana <73327110+AndresTrianaD@users.noreply.github.com>
2023-09-13 23:49:50 -0500

    Entrada de datos programa uno

---
+++ Programas Python/007 Entrada de datos1.py
@@ -0,0 +1,17 @@
+palabra = input ("Introduce una palabra: ")
+num_int = int (input("Introduce un numero entero: "))
+num_float = float(input("Introduce un numero flotante: "))
+#num_float solo es el nombre de la variable (=float) es el tipo de variable  
+num_compless = complex (input("Introduce un numero complejo: "))
+
+print ("String ", palabra)
+print ("Entero ", num_int)
+print ("Flotante ", num_float)
+print ("Complejo ", num_compless)
+
+'''palabra => string => Andres
+int => 12
+float => 3.5
+complex => 1 => (1+0j)
+'''
+
