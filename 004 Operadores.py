71659d50b88256efd1d6ddbe8f53867a359e4dcd
Andres Triana <73327110+AndresTrianaD@users.noreply.github.com>
2023-09-16 21:40:44 -0500

    ss

---
+++ 004 Operadores.py
@@ -0,0 +1,63 @@
+print ("Suma:")
+
+numero_uno = 5
+numero_dos = 4
+
+resultado = numero_uno + numero_dos
+
+print ("El resultado de la suma es:" + str(resultado))
+
+print ("Resta:")
+
+numero_uno = 5
+numero_dos = 4
+
+resultado = numero_uno - numero_dos
+
+print ("El resultado de la resta es:" + str(resultado))
+
+print ("Multiplicacion:")
+
+numero_uno = 5
+numero_dos = 4
+
+resultado = numero_uno * numero_dos
+
+print ("El resultado de la multiplicacion es:" + str(resultado))
+
+print ("Exponente:")
+
+numero_uno = 2
+exponente = 5
+
+resultado = numero_uno ** exponente
+
+print ("El resultado del exponente es:" + str(resultado))
+
+print ("Divicion:")
+
+numero_uno = 10
+numero_dos = 5
+
+resultado = numero_uno / numero_dos
+
+print ("El resultado de la divicion es:" + str(resultado))
+
+print ("Modulo:")
+
+numero_uno = 30
+numero_dos = 8
+
+resultado = numero_uno % numero_dos
+
+print ("El resultado de la modulo es:" + str(resultado))
+
+print ("Division entera:")
+
+numero_uno = 10
+numero_dos = 5
+
+resultado = numero_uno // numero_dos
+
+print ("El resultado de la Division entera es:" + str(resultado))
+
--- Programas Python/008 Entrada de datos2.py
+++
@@ -1,9 +0,0 @@
-nombre = input ("¿Cual es tu nombre?: ")
-print ("Hola " + nombre + ", vamos a realizar una suma.")
-
-num_uno = int (input("Por favor introduce el primer valor: "))
-num_dos = int (input("Por favor introduce el segundo valor: "))
-
-resultado = num_uno + num_dos
-#El (+) nos permite unir el strin. La (,) nos permite no convertir a string el int 
-print (nombre + " el resultado de la suma es: ", resultado)
