b99a5d144d3859c286c7bae25450385cba9f71f8
Andres Triana <73327110+AndresTrianaD@users.noreply.github.com>
2023-09-20 23:01:56 -0500

    Sentencias condicionales multiples_elif

---
+++ 011 Sentencias condicionales multiples_elif.py
@@ -0,0 +1,32 @@
+#Ejemplo 1:
+num_uno = 1
+if num_uno == 1:
+    print("El numero es Uno")
+elif num_uno == 2:
+    print("El numero es Dos")
+else:
+    print("El numero es desconoce")
+print("Fin.")
+'''
+Ejemplo 2
+'''
+print("===================================")
+print("¡¡Convertidor de números a letras!!")
+print("===================================")
+
+num_uno = int(input("¿Cúal es el número que deseas convertir?: "))
+
+if num_uno == 1:
+    print("El número es 'Uno'")
+elif num_uno == 2:
+    print("El número es 'Dos'")
+elif num_uno == 3:
+    print("El número es 'Tres'")
+elif num_uno == 4:
+    print("El número es 'Cuatro'")
+elif num_uno == 5:
+    print("El número es 'Cinco'")
+else:
+    print("Este programa solo puede convertir hasta el número 5")
+
+print("Fin.")
